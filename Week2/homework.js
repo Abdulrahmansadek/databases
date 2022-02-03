938dcd2e276a38abb9ac12e42f62443c376f4947
Abdulrahmansadek <boody.sad@gmail.com>
2022-02-03 12:10:25 +0100

    finish the homework week2

--- Week2/homework.js
+++ Week2/homework.js
@@ -13,6 +13,7 @@ const dbQueries = [
   `DROP DATABASE  homework`,
   `CREATE DATABASE homework`,
   `USE homework`,
+  `SET FOREIGN_KEY_CHECKS=0`,
 ];
 dbQueries.forEach((db) =>
   connection.query(db, (error, result) => {
@@ -55,15 +56,29 @@ const papersTable = [
         paper_id INT PRIMARY KEY AUTO_INCREMENT,
         paper_title VARCHAR (100),
         conference VARCHAR(100),
-        publish_date YEAR,
-        author_id INT,
-        FOREIGN KEY (author_id) REFERENCES authors( author_no))
+        publish_date YEAR)
      `,
 ];
 papersTable.forEach((table) =>
   connection.query(table, (error, result) => {
     if (error) throw error;
     console.log(`papers table is added`);
+  })
+);
+
+const createAuthorsResearchPapersTable = [
+  `CREATE TABLE author_research (
+    author_no INT NOT NULL,
+    paper_id INT NOT NULL,
+   FOREIGN KEY(author_no) REFERENCES authors(author_no),
+   FOREIGN KEY(paper_id) REFERENCES research_Papers(paper_id),
+   PRIMARY KEY (author_no,paper_id))
+`,
+];
+createAuthorsResearchPapersTable.forEach((table) =>
+  connection.query(table, (error, result) => {
+    if (error) throw error;
+    console.log(`authors research papers table is added`);
   })
 );
 
@@ -76,7 +91,7 @@ const authorsData = `INSERT INTO authors (author_name,
   gender,
   mentor) VALUES ('Smol JP','Canada university','1976-01-14',14,'M',5),
   ('Xie P A','china university','1969-01-12',6,'F',4),
-  ('Effler SW','usa University','1970-02-15',10,'M',3),
+  ('Effler  SW','usa University','1970-02-15',10,'M',3),
   ('Rodriguez-Iturbe I','University of Sevilla','1972-04-12',14,'M',2),
   ('Rinaldo A','Italy university','1963-07-12',14,'M',2),
   ('Hecky RE','Canada university','1977-09-02',14,'M',4),
@@ -94,33 +109,50 @@ const authorsData = `INSERT INTO authors (author_name,
 
 // insert data into research_papers
 
-const research_data = `INSERT INTO research_papers(paper_title,conference,publish_date,author_id)
+const research_data = `INSERT INTO research_papers(paper_title,conference,publish_date)
 VALUES 
-('weight control','How effective are different exercise regimes for losing weight and maintaining weight loss','2010',2),
-('stress','How does stress affect the body?','2010',2),
-('suicide','Should physician-assisted suicide be legal?','2010',3),
-('civil war','What were the causes of the Civil War?','2010',4),
-('roman empire','What events led to the fall of the Roman Empire?','2010',5),
-('black holes','How are black holes created?','2010',5),
-('nuclear energy','Should the US rely on nuclear energy more?','2010',5),
-('intelligence','How is Google search affecting our intelligence?','2010',8),
-('phones and productivity','Do smartphones increase or decrease workplace productivity?','2010',9),
-('social media','Has social media made people more or less connected?','2010',10),
-('auto pilot','Analyze the history and progress of self-driving vehicles.','2010',10),
-('NASA',"What have NASA's biggest successes and failures been?",'2010',11),
-('japan','How has Japan changed and evolved over the centuries?','2010',11),
-('cleopatra',"What led to Cleopatra's fall as ruler of Egypt?",'2010',14),
-('british rule in india','What were the impacts of British rule in India?','2010',15),
-('hiroshima and nagasaki','Was the atomic bombing of Hiroshima and Nagasaki necessary?','2010',14),
-('memories','Which methods are most effective for improving memory?','2010',8),
-('prisons','Which prison reforms have proven most effective?','2010',9),
-('us security airport','Has the increase in US airport security made passengers safer?','2010',10),
-('divorce rate','How and why have divorce rates changed over time?','2010',2),
-('education','Do students learn better in same-sex classrooms?','2010',1),
-('Brexis','What factors contributed to the UK deciding to leave the EU (Brexit)?','2010',14),
-('rap music','How has rap music evolved in the past decade?','2010',14),
-('famous artist','Analyze the impact a famous artist had on the world.','2010',15)
+('weight control','How effective are different exercise regimes for losing weight and maintaining weight loss','2010'),
+('stress','How does stress affect the body?','2010'),
+('suicide','Should physician-assisted suicide be legal?','2010'),
+('civil war','What were the causes of the Civil War?','2010'),
+('roman empire','What events led to the fall of the Roman Empire?','2010'),
+('black holes','How are black holes created?','2010'),
+('nuclear energy','Should the US rely on nuclear energy more?','2010'),
+('intelligence','How is Google search affecting our intelligence?','2010'),
+('phones and productivity','Do smartphones increase or decrease workplace productivity?','2010'),
+('social media','Has social media made people more or less connected?','2010'),
+('auto pilot','Analyze the history and progress of self-driving vehicles.','2010'),
+('NASA',"What have NASA's biggest successes and failures been?",'2010'),
+('japan','How has Japan changed and evolved over the centuries?','2010'),
+('cleopatra',"What led to Cleopatra's fall as ruler of Egypt?",'2010'),
+('british rule in india','What were the impacts of British rule in India?','2010'),
+('hiroshima and nagasaki','Was the atomic bombing of Hiroshima and Nagasaki necessary?','2010'),
+('memories','Which methods are most effective for improving memory?','2010'),
+('prisons','Which prison reforms have proven most effective?','2010'),
+('us security airport','Has the increase in US airport security made passengers safer?','2010'),
+('divorce rate','How and why have divorce rates changed over time?','2010'),
+('education','Do students learn better in same-sex classrooms?','2010'),
+('Brexis','What factors contributed to the UK deciding to leave the EU (Brexit)?','2010'),
+('rap music','How has rap music evolved in the past decade?','2010'),
+('famous artist','Analyze the impact a famous artist had on the world.','2010')
 `;
+
+// insert authors research data
+const authorsResearchData = `INSERT INTO author_research (author_no,paper_id)
+VALUES 
+(1,2),(1,3),(2,1),(3,4),(4,5),
+(10,6),(12,7),(15,8),
+(1,9),(2,10),(4,11),
+(10,12),(12,13),(5,14),
+(9,15),(9,16),(2,17),(1,18),
+(9,19),(9,20),(1,20),(2,22),(9,22)`;
+
+// update mentor
+const updateMentor = `ALTER TABLE authors ADD CONSTRAINT FOREIGN KEY(mentor) REFERENCES authors(author_no)`;
+connection.query(updateMentor, (error, result) => {
+  if (error) throw error;
+  console.log("mentor column updated");
+});
 
 //data function
 const insertData = function (data) {
@@ -132,16 +164,23 @@ const insertData = function (data) {
 
 insertData(authorsData);
 insertData(research_data);
+insertData(authorsResearchData);
 
 //1. Write a query that prints names of all `authors` and their corresponding `mentors`.
 //2. Write a query that prints all columns of `authors` and their published `paper_title`.
 //If there is an author without any `research_Papers`, print the information of that `author` too.
 
 const solutionQueries = [
-  `SELECT author_name,mentor FROM authors `,
-  `SELECT author_name , ifnull(paper_title,'no papers') FROM authors 
-LEFT JOIN research_papers 
-ON authors.author_no=research_papers.author_id`,
+  `SELECT a.author_name,m.author_name As mentor 
+  FROM authors a 
+  JOIN authors m
+  ON a.mentor=m.author_no`,
+  `SELECT a.* ,r.paper_title
+  FROM authors a
+  LEFT JOIN author_research re
+  ON a.author_no=re.author_no
+  LEFT JOIN research_papers r
+  ON r.paper_id=re.author_no`,
 ];
 
 solutionQueries.forEach((solution) => {
@@ -158,22 +197,30 @@ solutionQueries.forEach((solution) => {
 //5. Minimum and maximum of the h-index of all authors per university.
 
 const solutions_excercise2 = [
-  `SELECT author_no,author_name,paper_title FROM authors 
-  JOIN research_papers 
-  ON authors.author_no=research_papers.author_id`,
-  `SELECT count(*) AS 'published by female' FROM authors 
- JOIN research_papers 
-ON authors.author_no=research_papers.author_id
-WHERE gender ='F'`,
-  `SELECT university,AVG(h_index) AS 'average h-index' FROM authors group by university `,
-  `SELECT university,count(*) AS total
+  //All research papers and the number of authors that wrote that paper.
+  `SELECT paper_title,COUNT(1) As total FROM research_papers
+  LEFT JOIN author_research
+  ON research_papers.paper_id=author_research.paper_id
+  GROUP BY paper_title`,
+
+  //Sum of the research papers published by all female authors.
+  `SELECT count(1) AS 'published by female' FROM research_papers
+ JOIN author_research
+   ON research_papers.paper_id=author_research.paper_id
+  JOIN authors 
+  ON authors.author_no=author_research.author_no
+     WHERE gender ='F'`,
+
+  //Average of the h-index of all authors per university.
+  `SELECT university,AVG(h_index) AS 'average h-index' FROM authors GROUP BY university `,
+  //Sum of the research papers of the authors per university.
+  `SELECT university,count(paper_id) AS total
   FROM authors 
-   JOIN research_papers  
-   ON authors.author_no=research_papers.author_id 
+   JOIN  author_research
+   ON authors.author_no=author_research.author_no 
    GROUP by university ORDER BY total DESC`,
-  `SELECT MIN(h_index),MAX(h_index) FROM authors 
-  JOIN research_papers
-  ON  authors.author_no=research_papers.author_id 
+  //Minimum and maximum of the h-index of all authors per university.
+  `SELECT university,MIN(h_index),MAX(h_index) FROM authors 
   GROUP BY university`,
 ];
 
